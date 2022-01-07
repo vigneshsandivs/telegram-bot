57d146d98d0917b3b90a459f583dbd87e1078c7a
vigneshsandivs <97293125+vigneshsandivs@users.noreply.github.com>
2022-01-07 18:48:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
-<?php
-echo "<h2>PHP is Fun!</h2>";
-echo "Hello world!<br>";
-echo "I'm about to learn PHP!<br>";
-echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+<?php 
+$token = "5089228115:AAEYxlpIzTTWJGcjqJZmOnB3YdaXLZY9Rec";
+$link1 = "https://api.telegram.org/bot".$token;
+
+$updates = file_get_contents('php://input');
+$updates = json_decode($updates, TRUE);
+
+$msgID = $updates['message']['chat']['id'];
+$name = $updates['message']['chat']['first_name'];
+$text = $updates['message']['text'];
+
+switch ($text) {
+	case"start":
+		sendmsg($msgID, "Welcome $name");
+		break;
+	case"bye":
+	sendmsg($msgID, "Nice To See You");
+	sendmsg($msgID, "\xF0\x9F\x98\x98");
+	break;
+	
+}
+function sendmsg($msgID, $text)
+{
+	$url = $GLOBALS[link1].'/sendMessage?chat_id='.$msgID.'&text='.urlencode($text);
+	file_get_contents($url);
+}
 ?>
